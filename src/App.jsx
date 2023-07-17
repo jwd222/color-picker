04135b158f97de1cf94d6660de09331625c6ccb9
jwd222 <sji.2.sami@gmail.com>
2023-07-18 03:45:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { ColorList } from './ColorList'
+import { Form } from './Form'
+import Values from 'values.js'
+import { ToastContainer, toast } from 'react-toastify'
+ToastContainer
+
+const App = () => {
+  const [colors, setColors] = useState(new Values('#f15025').all(10))
+
+  return (
+    <main>
+      <Form />
+      <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
+    </main>
+  )
+}
+export default App
